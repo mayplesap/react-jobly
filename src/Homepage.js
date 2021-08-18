5173efe553bd2d01edafb5a14b521f9a82c455c0
magus <harvestashya@gmail.com>
2021-08-18 15:56:56 -0700

    homepage stuff

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,17 +1,26 @@
+import { Link } from 'react-router-dom';
+import { useContext } from "react";
+import UserContext from './userContext';
+
 function Homepage() {
+  const user = useContext(UserContext); 
 
   return (
     <div className="text-center mt-5">
-      <h1>Welcome Back</h1>
+      <h1>Jobly</h1>
+      
+      {user? :
+      
+        }
       <li>
-        <NavLink exact to="/login" className="nav-item nav-link">
+        <Link exact to="/login" className="nav-item nav-link">
           Login
-        </NavLink>
+        </Link>
       </li>
       <li>
-        <NavLink exact to="/signup" className="nav-item nav-link">
+        <Link exact to="/signup" className="nav-item nav-link">
           Signup
-        </NavLink>
+        </Link>
       </li>
     </div>
   )
